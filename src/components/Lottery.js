98f804a58f818cabfc23dee28257b7d19b8b30fc
Ryan Johnston <ryjo1026@gmail.com>
2018-05-23 18:02:12 +0200

    Initial support for multiple tickets

--- src/components/Lottery.js
+++ src/components/Lottery.js
@@ -1,12 +1,14 @@
 import React from 'react';
+import contract from 'truffle-contract';
 import PropTypes from 'prop-types';
 import {withStyles} from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import CardActions from '@material-ui/core/CardActions';
 import Typography from '@material-ui/core/Typography';
-
+import BigNumber from 'bignumber.js';
 
 const styles = {
   card: {
@@ -26,32 +28,19 @@ class Lottery extends React.Component {
   constructor(props, context) {
     super(props);
     this.state = {
-        ticketPrice: 1,
-        account: '',
+        ticketAmount: 1,
+        buttonText: 'Buy a Ticket for ether',
+        buttonEnabled: false,
     };
 
-    // Get pre-built JSON interface from truffle
+    this.updateTicketAmount = this.updateTicketAmount.bind(this);
+
+    // Get pre-built JSON ABI TODO change to grabbing published from web
     let contractJson = require('../build/contracts/LotterEth.json');
-    //
-    let contract = require('truffle-contract');
     this.LotterEth = contract(contractJson);
     this.LotterEth.setProvider(this.props.web3.currentProvider);
 
-    // this.address = '0x533e7693b92e0c77cd6c148dcbcc92f47ebbf980';
-    this.address = '0x1a0f0e2c3c1ad06c449b9f4f5448060a9ad48d16'
-
-    // this.LotterEth = new this.props.web3.eth.Contract(contractJson.abi,
-    //   '0x533e7693b92e0c77cd6c148dcbcc92f47ebbf980');
-    // this.LotterEth.methods.ticketsBought().call().then((result)=>console.log(result));
-
-    this.LotterEth.at(this.address).then((at) => {
-      at.ticketsBought().then((newParticipants) => {
-        console.log(newParticipants);
-        this.setState({
-          lotteryParticipants: newParticipants.toString(),
-        });
-      });
-    });
+    this.address = '0x1a0f0e2c3c1ad06c449b9f4f5448060a9ad48d16';
   }
 
   updateLotteryParticipants() {
@@ -78,19 +67,23 @@ class Lottery extends React.Component {
   }
 
   buyTicket() {
+    let x = new BigNumber(this.state.ticketAmount);
+    let price = this.state.ticketPriceRaw * x;
     this.props.web3.eth.sendTransaction({
       from: this.state.account,
       to: this.address,
-      value: this.state.ticketPriceRaw});
+      value: price});
     this.updateLotteryParticipants();
   }
 
   componentWillMount() {
     this.LotterEth.at(this.address).then((at) => {
       at.ticketPrice().then((price) => {
+        let convert = this.props.web3.utils.fromWei(price.toString(), 'ether');
         this.setState({
           ticketPriceRaw: price,
-          ticketPrice: this.props.web3.utils.fromWei(price.toString(), 'ether'),
+          ticketPrice: convert,
+          buttonText: 'Buy a Ticket for '+convert.toString()+' ether',
         });
       });
     });
@@ -105,6 +98,41 @@ class Lottery extends React.Component {
     }, 1000);
   }
 
+  updateTicketAmount(e) {
+    if (e.target.value < 0) {
+      this.setState({
+        ticketAmount: 1,
+        buttonText: 'Buy a Ticket for '+this.state.ticketPrice+' ether',
+        buttonEnabled: false,
+      });
+    } else if (e.target.value === '' || Number(e.target.value) === 0) {
+      this.setState({
+        ticketAmount: e.target.value,
+        buttonText: 'Buy '+ 0
+          +' Tickets for '
+          + 0
+          +' ether',
+        buttonEnabled: true,
+      });
+    } else if (e.target.value % 1 !== 0) {
+      this.setState({
+        ticketAmount: Math.floor(e.target.value),
+        buttonEnabled: false,
+      });
+    } else {
+      let x = new BigNumber(Number(e.target.value));
+      let y = new BigNumber(1000000000000000000);
+      this.setState({
+        ticketAmount: e.target.value,
+        buttonText: 'Buy '+ e.target.value
+          +' Tickets for '
+          + (x/y) * this.state.ticketPriceRaw
+          +' ether',
+        buttonEnabled: false,
+      });
+    }
+  }
+
   render() {
     const {classes} = this.props;
     return (
@@ -116,11 +144,24 @@ class Lottery extends React.Component {
           <Typography component="p">
             {this.state.lotteryParticipants} / {this.state.jackpotNeeded}
           </Typography>
+          <TextField
+              id="ticket-amount"
+              label="Tickets to buy"
+              value={this.state.ticketAmount}
+              onChange={this.updateTicketAmount}
+              type="number"
+              className={classes.textField}
+              InputLabelProps={{
+                shrink: true,
+              }}
+              margin="normal"
+          />
         </CardContent>
         <CardActions>
           <Button variant="raised" color="primary" className={classes.button}
-          onClick={ () => this.buyTicket()}>
-            Buy a Ticket for {this.state.ticketPrice} ether
+            disabled={this.state.buttonEnabled}
+            onClick={ () => this.buyTicket()}>
+            {this.state.buttonText}
           </Button>
         </CardActions>
       </Card>
